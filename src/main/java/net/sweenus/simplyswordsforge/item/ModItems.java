1b02416a90a9528fc2df10e9ea1069c7f2303863
Sweenus <48658188+Sweenus@users.noreply.github.com>
2022-08-29 11:45:38 +1000

    Initial commit

---
+++ src/main/java/net/sweenus/simplyswordsforge/item/ModItems.java
@@ -0,0 +1,13 @@
+package net.sweenus.simplyswordsforge.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.sweenus.simplyswordsforge.SimplySwords;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SimplySwords.MOD_ID);
+
+
+}
